e01ae6a205a56b92415ae02046d9d77d14b6f6a8
Kadeem140 <Beckford.kadeem@yahoo.com>
2022-02-22 21:27:20 -0500

    MVP

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 
+function superbowlWin(arr){
+    const findWin = arr.find((record) => {
+        return record.result === 'W'
+    })
+    if(!findWin){
+        return undefined
+    }
+    console.log(findWin.year, "ANSWER!")
+    return findWin.year
+}
